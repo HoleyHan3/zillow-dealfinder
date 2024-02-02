860c435d8d73cd482f56d40e22f0b654c528e0dd
HoleyHan3 <128749611+HoleyHan3@users.noreply.github.com>
2024-02-01 20:02:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,64 +3,18 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 import lxml
+import re
 from lxml.html.soupparser import fromstring
 # Configure logging
 import logging
 
+# Define constants
+SALE_OR_RENT_OPTIONS = ["For Sale", "For Rent"]
+HOME_TYPE_OPTIONS = ["Houses", "Apartments", "Townhomes"]
+
 # Set up logging configuration
 logging.basicConfig(level=logging.INFO)
 
-# Function to validate location input
-def validate_location(location):
-    # Check if location contains only letters, spaces, and hyphens
-    if not re.match("^[a-zA-Z\s\-0-9]+$", location):
-        return False
-    return True
-    
-# Function to format the search parameter based on user input
-def format_search_parameter(user_input):
-    # Replace spaces with hyphens
-    formatted_parameter = user_input.strip().lower().replace(" ", "-")
-    return formatted_parameter
-
-@st.cache(show_spinner=False)
-# Function to fetch data from Zillow
-def fetch_zillow_data(location, sale_or_rent='', home_type=''):
-    # Define the base URL
-    base_url = 'https://www.zillow.com/'
-
-    # Construct the URL based on the provided parameters
-    url = f'{base_url}{location}/{home_type}/{sale_or_rent}/'
-
-    # Define request headers
-    request_headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'en-US,en;q=0.8',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
-    }
-
-    # Send HTTP request
-    with requests.Session() as session:
-        response = session.get(url, headers=request_headers)
-
-    # Parse HTML content
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Extract property details
-    addresses = [address.get_text(strip=True) for address in soup.find_all(class_='list-card-addr')]
-    prices = [price.get_text(strip=True) for price in soup.find_all(class_='list-card-price')]
-    beds = [bed.get_text(strip=True) for bed in soup.find_all('ul', class_='list-card-details')]
-    links = [link['href'] for link in soup.find_all(class_='list-card-link')]
-
-    # Create DataFrame
-    df = pd.DataFrame({'Address': addresses, 
-                       'Price': prices, 
-                       'Beds': beds, 
-                       'Link': links})
-
-    return df
 
 # Streamlit App
 def main():
@@ -75,28 +29,118 @@ def main():
         return
 
     # Select box for choosing between homes for sale and homes for rent
-    sale_or_rent = st.selectbox("Select", ["For Sale", "For Rent"])
-        # Map the selection to the corresponding URL parameter
-        if sale_or_rent == "For Sale":
-            sale_or_rent_param = "for_sale"
-        else:
-            sale_or_rent_param = "for_rent"
+    sale_or_rent = st.selectbox("Select", SALE_OR_RENT_OPTIONS,index=0)
+
     # Select box for choosing home types
-    home_type = st.selectbox("Select Home Type", ["Houses", "Apartments", "Townhomes"])
+    home_type = st.selectbox("Select Home Type", HOME_TYPE_OPTIONS,index=0)
 
-   # Button to fetch data
+    # Select box for choosing the number of listings
+    num_listings = st.selectbox("Select Number of Listings", [5, 10, 50, 100],index=0)
+
+    # Button to fetch data
     if st.button('Fetch Zillow Data'):
-        if city.strip() == "":
-            st.warning("Please enter a valid city.")
+        zillow_df = fetch_zillow_data(location, sale_or_rent.lower(), home_type.lower(), num_listings)
+        if zillow_df.empty:
+            st.warning("No data available. Please refine your search criteria.")
         else:
-            zillow_df = fetch_zillow_data(city, sale_or_rent_param, home_type)
+            # Format HTML for display
+            html = zillow_df.to_html(escape=False, index=False)
+            st.write(html, unsafe_allow_html=True)
 
-            if zillow_df.empty:
-                st.warning("No data available. Please refine your search criteria.")
-            else:
-                # Format HTML for display
-                html = zillow_df.to_html(escape=False, index=False)
-                st.write(html, unsafe_allow_html=True)
+# Function to validate location input
+def validate_location(location):
+    # Check if location contains only letters, spaces, and hyphens
+    if not re.match("^[a-zA-Z\s\-0-9]+$", location):
+        return False
+    return True
+
+# Function to format the search parameter based on user input
+def format_search_parameter(user_input):
+    # Replace spaces with hyphens
+    formatted_parameter = user_input.strip().lower().replace(" ", "-")
+    return formatted_parameter
+ 
+
+# Function to fetch data from Zillow
+@st.cache(show_spinner=False)
+def fetch_zillow_data(location, sale_or_rent='', home_type='', max_properties=None):
+    try:
+        # Define the base URL
+        base_url = f'https://www.zillow.com/{location}/{home_type}/{sale_or_rent}/'
+
+        # Define request headers
+        request_headers = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-US,en;q=0.8',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+        }
+
+        # List to store property details
+        all_addresses = []
+        all_prices = []
+        all_beds = []
+        all_links = []
+
+        # Iterate through pages
+        page_num = 1
+        properties_fetched = 0
+        while True:
+            url = f'{base_url}{page_num}_p/' if page_num > 1 else base_url
+            with requests.Session() as session:
+                response = session.get(url, headers=request_headers)
+
+            # Check if page exists
+            if response.status_code != 200:
+                break
+
+            # Parse HTML content
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            # Extract property details
+            addresses = [address.get_text(strip=True) for address in soup.find_all(class_='list-card-addr')]
+            prices = [price.get_text(strip=True) for price in soup.find_all(class_='list-card-price')]
+            beds = [bed.get_text(strip=True) for bed in soup.find_all('ul', class_='list-card-details')]
+            links = [link['href'] for link in soup.find_all(class_='list-card-link')]
+
+            # Calculate the number of new properties fetched
+            num_new_properties = len(addresses)
+
+            # If the number of properties fetched exceeds the maximum or if max_properties is specified
+            if max_properties is not None and properties_fetched + num_new_properties > max_properties:
+                num_new_properties = max_properties - properties_fetched
+                addresses = addresses[:num_new_properties]
+                prices = prices[:num_new_properties]
+                beds = beds[:num_new_properties]
+                links = links[:num_new_properties]
+
+            # Append details to the lists
+            all_addresses.extend(addresses)
+            all_prices.extend(prices)
+            all_beds.extend(beds)
+            all_links.extend(links)
+
+            # Update the total number of properties fetched
+            properties_fetched += num_new_properties
+
+            # Check if the maximum number of properties has been reached
+            if max_properties is not None and properties_fetched >= max_properties:
+                break
+
+            page_num += 1
+
+        # Create DataFrame
+        df = pd.DataFrame({'Address': all_addresses,
+                           'Price': all_prices,
+                           'Beds': all_beds,
+                           'Link': all_links})
+
+        return df
+
+    except Exception as e:
+        logging.error(f"An error occurred while fetching data: {str(e)}")
+        return pd.DataFrame()
 
 if __name__ == "__main__":
     main()
